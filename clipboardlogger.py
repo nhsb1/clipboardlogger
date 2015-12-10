3da5c191ee7904f07c61566fed8408444dfaca28
nhsb1 <nhsb1@users.noreply.github.com>
2015-12-09 21:09:18 -0500

    Update clipboardlogger.py

--- clipboardlogger.py
+++ clipboardlogger.py
@@ -4,22 +4,35 @@ import datetime
 import sys
 import os
 from sys import argv
+from argparse import ArgumentParser
 
-script, filename = argv
-target = open(filename, 'w')
+#Clipboard to Text File - saves clipboard items to an arbitrary text file, appending
 
-init_value = ""
-print "Initalizing clipboard with null value... "
-print "(Paste events echo'd to console, overwriting %r; CTRL-C to exit)" %filename
-newnow = datetime.datetime.now()
-pyperclip.copy("")
+parser = ArgumentParser(description = 'cxz - copy paste to text')
+parser.add_argument("-f", "--file", dest="filename", help="file name to write output", metavar="FILE")
+parser.add_argument("-s", "--save", action="store_true", dest="savetrue", default=False, help="save current string to clipboard")
+parser.add_argument("-c", "--current", action="store_true", dest="clipboardnow", default=False, help="print current clipboard content")
+parser.add_argument("-r", "--read", action="store_true", dest="readfile", default=False, help="print the content of the specified file")
+args = parser.parse_args()
 
-while True:
-    i = input("")
-    current_paste = pyperclip.paste()
-    if current_paste != init_value:
-        init_value = current_paste
-        print "Clipboard Changed: %s" % str(current_paste)[:1000]
-        target.write(current_paste)
-        target.write("\n")
-    time.sleep(0.1)
+if args.clipboardnow:
+	currentclipboard = pyperclip.paste()
+	print currentclipboard
+
+if args.savetrue:
+	if args.filename:
+		currentclipboard = pyperclip.paste()
+		target = open(args.filename, "a")
+		target.write(currentclipboard)
+		target.write("\n")
+		print currentclipboard
+	else:
+		print "Please specify an output file.  -f filename.txt"
+
+if args.readfile:
+	if args.filename:
+		target = open(args.filename)
+		print target.read()
+	else:
+		print "Please specify a file to read.  -f filename.txt"		
+
