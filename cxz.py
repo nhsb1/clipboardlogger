de0e9136d48917c26821aa7a3be12950c6ec738d
nhsb1 <nhsb1@users.noreply.github.com>
2016-03-18 13:51:19 -0400

    Update cxz.py
    version .26

--- cxz.py
+++ cxz.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser
 import pyperclip
 import datetime
 
-
+#.26
 #Clipboard to Text File - saves clipboard items to an arbitrary text file, appending
 #Dependencies: pyhk, pyhook, pywin32, sys, argparse, pyperclip, datetime
 #
@@ -13,9 +13,17 @@ import datetime
 #pyHook, pyWin32 available here: http://www.lfd.uci.edu/~gohlke/pythonlibs/
 #To install: pip install *.whl 
 #Make sure to run: python.exe Scripts\pywin32_postinstall.py -install (from cmd launched as administrator)
+#Feature request:
+#1. timestamp on content changes
+#2. Clear all (?) clipboard lists
+#3. Get most recent item out of list and back to clipboard?
+#4. Add E and R to clipboard lists; add O and P to clipboard DIR list
+#5. Load hotkeys from config file - user editable?
 
 cliplistq = []
 cliplistw=[]
+clipliste=[]
+cliplistr=[]
 newnow = ""
 
 parser = ArgumentParser(description = 'cxz - copy paste to text')
@@ -26,13 +34,13 @@ args = parser.parse_args()
 def blankfile():
 	global newnow
 	newnow = str(datetime.datetime.now().date()) + ".txt"
+	print "-------------------------------------------------------------------------------"
 	print "No filename specified.  Next time, use (-f filename.txt), saving to: " + newnow
-	print "Save current clipboard content to file: CTRL+ALT+1"
-	print "Save clipboard to list Q: CTRL+ALT+Q"
-	print "Exit: CTRL+ALT+0"
-	print "Show content of current save file: CTRL+ALT+R."
+	print "Active Clipboards - hotkey: CTRL+ALT+Q,W,E,R"
+	print "List Clipboard content - hotkey: CTRL+ALT+U,I,O,P"
+	print "Save all lists to current file - hotkey: CTRL+ALT+S"
+	print "Load/Print active save file to console - hotkey: CTRL+ALT+L"
 	print "Show help: CTRL+ALT+H"
-	print "Commit List Q to savefile"
 	print "-------------------------------------------------------------------------------"
 	args.filename=newnow
 
@@ -79,6 +87,39 @@ def wlistcontent():
 	for i in cliplistw:
 		print i
 
+
+def myliste(): #add current clipboiard to my temporary list
+	tmpclip = ""
+	tmpclip = pyperclip.paste()
+	tmpclip = tmpclip.encode('utf-8')
+	tmpclip = tmpclip.strip()
+	global clipliste
+	clipliste.append(tmpclip)
+	print "Clipboard  added to templistE>"
+
+
+def olistcontent():
+	#print ">>"
+	global clipliste
+	for i in clipliste:
+		print i
+
+
+def mylistr(): #add current clipboiard to my temporary list
+	tmpclip = ""
+	tmpclip = pyperclip.paste()
+	tmpclip = tmpclip.encode('utf-8')
+	tmpclip = tmpclip.strip()
+	global cliplistr
+	cliplistr.append(tmpclip)
+	print "Clipboard  added to templistE>"
+
+
+def plistcontent():
+	#print ">>"
+	global cliplistr
+	for i in cliplistr:
+		print i
 	
 def savefile():
 	global cliplistq
@@ -96,11 +137,24 @@ def savefile():
 		target = open(args.filename, "a") #opens the file for appending
 		target.write(currentclipboard)
 		target.write("\n")
+	global clipliste
+	for i in clipliste:
+		currentclipboard = i
+		target = open(args.filename, "a") #opens the file for appending
+		target.write(currentclipboard)
+		target.write("\n")
+	global cliplistr
+	for i in cliplistr:
+		currentclipboard = i
+		target = open(args.filename, "a") #opens the file for appending
+		target.write(currentclipboard)
+		target.write("\n")	
 	print "Temporary Lists Committed to savefile>"
-	global cliplistq, cliplistw
+	#global cliplistq, cliplistw
 	cliplistq=[]
 	cliplistw=[]
-
+	clipliste=[]
+	cliplistr=[]
 
 
 
@@ -116,7 +170,7 @@ def savepaste(): #
 		print "Clipboard content committed to savefile>"
 
 def terminate():
-	print newnow
+	print "Save To: %r" %newnow
 	#sys.exit(1) 
 	os._exit(1)
 
@@ -124,15 +178,19 @@ def terminate():
 hot = pyhk.pyhk()
 
 #add hotkey
-hot.addHotkey(['Ctrl', 'Alt', '1'],savepaste)
+hot.addHotkey(['Ctrl', 'Alt', 'S'],savepaste)
 hot.addHotkey(['Ctrl', 'Alt', 'H'],blankfile)
-hot.addHotkey(['Ctrl', 'Alt', 'R'],readfile)
+hot.addHotkey(['Ctrl', 'Alt', 'L'],readfile)
 hot.addHotkey(['Ctrl', 'Alt', 'S'],savefile)
 hot.addHotkey(['Ctrl', 'Alt', '0'],terminate)
 hot.addHotkey(['Ctrl', 'Alt', 'Q'],mylistq)
 hot.addHotkey(['Ctrl', 'Alt', 'U'],qlistcontent) 
 hot.addHotkey(['Ctrl', 'Alt', 'W'],mylistw)
 hot.addHotkey(['Ctrl', 'Alt', 'I'],wlistcontent) 
+hot.addHotkey(['Ctrl', 'Alt', 'E'],myliste)
+hot.addHotkey(['Ctrl', 'Alt', 'O'],olistcontent) 
+hot.addHotkey(['Ctrl', 'Alt', 'R'],mylistr)
+hot.addHotkey(['Ctrl', 'Alt', 'P'],plistcontent) 
 
 
 if args.filename:
